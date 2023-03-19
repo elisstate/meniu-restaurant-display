a89ad8b0647a2e4d6ea23c80cbbb9cc7f789cdd2
elisstate <elis.state@gmail.com>
2023-03-19 19:24:45 +0200

    fix width on phone

--- src/pdf/all-pages.js
+++ src/pdf/all-pages.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 import { Document, Page } from "react-pdf";
 import { Typography, Box, CircularProgress } from "@mui/material";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import './all-pages.css';
+
+const useWidth = (target) => {
+    const [width, setWidth] = useState(null);
+  
+    useLayoutEffect(() => {
+      setWidth(target.current.getBoundingClientRect().width)
+    }, [target]);
+
+    return width;
+  };
 
 export default function AllPages(props) {
   const [numPages, setNumPages] = useState(null);
-
+  const wrapperDiv = useRef(null);
+  const width = useWidth(wrapperDiv);
+  
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
@@ -24,17 +37,20 @@ export default function AllPages(props) {
   }
 
   return (
-    <Document
-        file={pdf}
-        options={{ workerSrc: "pdf.worker.js" }}
-        loading={loadingComp()}
-        onLoadSuccess={onDocumentLoadSuccess}
-        noData={loadingComp()}
-    >
-      {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} loading={loadingComp()}
-        fillWidth={true}/>
-      ))}
-    </Document>
+    <div ref={wrapperDiv} style={{width: '95vw'}}>
+        <Document
+            file={pdf}
+            className="page-pdf"
+            options={{ workerSrc: "pdf.worker.js" }}
+            loading={loadingComp()}
+            onLoadSuccess={onDocumentLoadSuccess}
+            noData={loadingComp()}
+        >
+        {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} loading={loadingComp()} width={width}
+            renderTextLayer={false}/>
+        ))}
+        </Document>
+    </div>
   );
 }
