01a33cf3e0a75fbd3406580a51e4c969a57050e2
elisstate <elis.state@gmail.com>
2025-04-08 07:40:03 +0300

    modify frozen product* placement

--- src/menu/MenuSection.js
+++ src/menu/MenuSection.js
@@ -1,19 +1,40 @@
-import * as React from 'react';
+import * as React from "react";
 import { Paper } from "@mui/material";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 import MenuItemDisplay from "./MenuItemDisplay";
+import { frozenIngredients } from "../service/menuData";
 
-const PRODUS_CONGELAT = "Produs congelat"
+const PRODUS_CONGELAT = "Produs congelat";
 export default function MenuSection(props) {
+  const isFrozenIngredientPresent = (entries) => {
+    return !!entries.find((entry) =>
+      entry.ingredients.find((ing) => frozenIngredients.includes(ing))
+    );
+  };
 
-    return <Paper elevation={4} outlined square={false} style={{marginBottom: 20, textAlign: 'left', paddingLeft: 40, paddingRight: 40}}>
-        <Typography variant="h3" paddingY={4}>
-            {props.category}
-        </Typography>
+  return (
+    <Paper
+      elevation={4}
+      outlined
+      square={false}
+      style={{
+        marginBottom: 20,
+        textAlign: "left",
+      }}
+    >
+      <Typography variant="h3" paddingY={4} style={{textAlign: 'center'}}>
+        {props.category}
+      </Typography>
 
-        {props?.entries.map(elem =>  <MenuItemDisplay {...elem} />)}
-        <div>
-        <sup>*</sup>{PRODUS_CONGELAT}
+      {props?.entries.map((elem) => (
+        <MenuItemDisplay {...elem} />
+      ))}
+      {isFrozenIngredientPresent(props.entries) && (
+        <div style={{paddingTop: 10, paddingBottom: 10, paddingLeft: 20}}>
+          <sup>*</sup>
+          {PRODUS_CONGELAT}
         </div>
+      )}
     </Paper>
+  );
 }
